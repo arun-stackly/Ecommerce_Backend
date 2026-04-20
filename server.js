1454216e7f2c1c1f17beaafb881c62f6c44786c2
Maha1010stackly <MahalakshmiManikandan@thestackly.com>
2026-04-20 13:32:33 +0530

    seller ads page mediaurl size updated

--- server.js
+++ server.js
@@ -56,7 +56,8 @@ app.use(morgan("dev"));
 app.use("/api/products", require("./routes/productRoutes"));
 app.use("/api/productitems", productitemRoutes);
 app.use("/api/productspecs", specificationRoutes);
-
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ limit: "20mb", extended: true }));
 app.use("/api/deals", require("./routes/dealRoutes"));
 app.use("/api/banners", require("./routes/bannerRoutes"));
 app.use("/api/home", require("./routes/homeRoutes"));
