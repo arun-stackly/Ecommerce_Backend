4244c9148b0372c1a8cd502159dba5d2f9a71294
Maha1010stackly <MahalakshmiManikandan@thestackly.com>
2026-05-08 14:04:17 +0530

    User Module cart, order, wishlist Updated

--- models/UserOrder.js
+++ models/UserOrder.js
@@ -11,11 +11,11 @@ const orderItemSchema = new mongoose.Schema({
     ref: "SellerInventory",
     required: true,
   },
-  productId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Product",
-    required: true,
-  },
+  // productId: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: "Product",
+  //   required: true,
+  // },
   name: { type: String, required: true },
   image: { type: String, required: true },
   price: { type: Number, required: true },
