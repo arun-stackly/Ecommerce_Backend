24630b66cca8a93079ba4203151dbba48cb021db
arun-stackly <arun@thestackly.com>
2026-06-16 16:08:38 +0530

    Seller quantity api added

--- routes/sellerInventoryRoutes.js
+++ routes/sellerInventoryRoutes.js
@@ -1,24 +1,33 @@
 const express = require("express");
 const router = express.Router();
-
+ 
 const { protect } = require("../middleware/authMiddleware");
 const { sellerOnly } = require("../middleware/roleMiddleware");
-
+ 
 const {
   createInventoryItem,
   getInventory,
   updateInventoryItem,
   deleteInventoryItem,
   getInventoryById,
+  updateInventoryStock,
 } = require("../controllers/sellerInventoryController");
-
+ 
 router.use(protect);
 router.use(sellerOnly);
-
+ 
 router.post("/", createInventoryItem);
+ 
 router.get("/", getInventory);
+ 
 router.get("/:id", getInventoryById);
+ 
 router.put("/:id", updateInventoryItem);
+ 
+router.patch("/:id/update-stock", updateInventoryStock);
+ 
 router.delete("/:id", deleteInventoryItem);
-
+ 
 module.exports = router;
+ 
+ 
