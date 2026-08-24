1094b3a09259d236cf28ef07935bb6b3b00fc0aa
arun-stackly <arun@thestackly.com>
2026-08-24 15:53:20 +0530

    sales report api protection added

--- routes/adminSalesreportRoutes.js
+++ routes/adminSalesreportRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/adminsalesReportController");
-
+const adminAuthMiddleware = require("../middleware/adminAuthMiddleware");
 // Sales Reports & Analytics
-router.get("/", controller.getSalesAnalytics);
+router.get("/",adminAuthMiddleware, controller.getSalesAnalytics);
 
 // Export Report
-router.get("/export", controller.exportSalesReport);
+router.get("/export",adminAuthMiddleware, controller.exportSalesReport);
 
 module.exports = router;
