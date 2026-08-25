34a5781434ce579d66e9ff449935b3302344026d
arun-stackly <arun@thestackly.com>
2026-08-25 10:18:27 +0530

    removed protection for all get apis in categories

--- routes/subcategories.js
+++ routes/subcategories.js
@@ -4,7 +4,7 @@ const ctrl = require('../controllers/subcategoriesController');
 const adminAuthMiddleware = require("../middleware/adminAuthMiddleware");
 
 router.post('/:category', adminAuthMiddleware, ctrl.createSubcategoriesForCategory);
-router.get('/:category', adminAuthMiddleware, ctrl.getSubcategoriesForCategory);
+router.get('/:category', ctrl.getSubcategoriesForCategory);
 router.put('/:id', adminAuthMiddleware, ctrl.updateSubcategory);
 router.delete('/:id', adminAuthMiddleware, ctrl.deleteSubcategory);
 
